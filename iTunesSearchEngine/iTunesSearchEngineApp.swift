ca9cd7e1a777c62470a17bbb6be907a5f3b4ef4a
Caleb <calebwongkaho1997@gmail.com>
2023-09-22 14:16:58 +0800

    Initial Commit

---
+++ iTunesSearchEngine/iTunesSearchEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchEngineApp.swift
+//  iTunesSearchEngine
+//
+//  Created by Caleb Wong on 22/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearchEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
